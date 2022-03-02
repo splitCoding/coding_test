d3a3934cf554aab8e50b034847cf7c1fec12fe2c
splitube <voicesplit@daum.net>
2022-03-02 11:23:08 +0900

    N과M(4) 문제 풀이 완료
    N과M(3)에 selected배열에 이전 값이 cand보다  작거나 같은 경우만 수행한다는 조건문 추가

---
+++ src/beakjoon/Prob_15652.java
@@ -0,0 +1,36 @@
+package beakjoon;
+
+public class Prob_15652 {
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        input();
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+    }
+
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i < M + 1; i++) sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        } else {
+            for (int cand = 1; cand < N + 1; cand++) {
+                if (selected[k - 1] <= cand) {
+                    selected[k] = cand;
+                    rec_func(k + 1);
+                    selected[k] = 0;
+                }
+            }
+        }
+
+    }
+}
