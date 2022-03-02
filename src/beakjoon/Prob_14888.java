4cb77376fd01f0655fa34ad4f3d3acb762942797
splitube <voicesplit@daum.net>
2022-03-02 13:03:06 +0900

    연산자 끼워넣기 문제 풀이 완료
    피연산자2개와 연산자 인덱스를 매개변수로 받아 계산해주는 calculate 함수, 이전 결과와 재귀된 횟수를 매개변수로 받는 재귀함수로 문제 해결

---
+++ src/beakjoon/Prob_14888.java
@@ -0,0 +1,55 @@
+package beakjoon;
+
+public class Prob_14888 {
+    static int N;
+    static int[] numbers, ops;
+    static int MAX = Integer.MIN_VALUE;
+    static int MIN = Integer.MAX_VALUE;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        input();
+        rec_func(1, numbers[0]);
+        sb.append(MAX).append('\n').append(MIN);
+        System.out.println(sb.toString());
+    }
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        numbers = new int[N];
+        ops = new int[4];
+        for (int i = 0; i < N; i++) numbers[i] = scan.nextInt();
+        for (int i = 0; i < 4; i++) ops[i] = scan.nextInt();
+    }
+
+    static int calculate(int num1, int op, int num2) {
+        if (op == 0) {
+            return num1 + num2;
+        } else if (op == 1) {
+            return num1 - num2;
+        } else if (op == 2) {
+            return num1 * num2;
+        } else {
+            return num1 / num2;
+        }
+    }
+
+    static void rec_func(int k, int result) {
+        if (k == N) {
+            if( MAX < result || MIN > result){
+                MIN = Math.min(MIN, result);
+                MAX = Math.max(MAX, result);
+            }
+        } else {
+            for (int i = 0; i < ops.length; i++) {
+                if (ops[i] > 0) {
+                    ops[i]--;
+                    rec_func(k + 1, calculate(result, i, numbers[k]));
+                    ops[i]++;
+                }
+            }
+        }
+    }
+}
+
