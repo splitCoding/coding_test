4e18294daadb87c60eb5cc292bea9ea14cf62d1e
splitube <voicesplit@daum.net>
2022-03-02 10:56:43 +0900

    N과M(3) 문제 풀이 완료
    재귀함수를 이용하여 풀이

--- src/beakjoon/Prob_15651.java
+++ src/beakjoon/Prob_15651.java
@@ -1,4 +1,35 @@
 package beakjoon;
 
 public class Prob_15651 {
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        input();
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+    }
+
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i < M + 1; i++) sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        } else {
+            for (int cand = 1; cand < N + 1; cand++) {
+                selected[k] = cand;
+                rec_func(k + 1);
+                selected[k] = 0;
+            }
+        }
+
+    }
+
 }
