7f0571460ac568f221d81748b47986c53e079132
splitube <voicesplit@daum.net>
2022-04-11 10:31:42 +0900

    수들의 합2 문제 풀이 완료 ( 피드백 전 )
    L부터 시작한 숫자들의 합이 M보다 같거나 커지면 멈추는 반복문을 반복한다. 반복문이 끝났을때 합이 M이면 count++ 한뒤 L을 합에서 뺀뒤 L++하여 L < N까지 반복한다.

---
+++ src/beakjoon/Prob_2003.java
@@ -0,0 +1,74 @@
+package beakjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Prob_2003 {
+    static int N, M, A[], sum = 0, count = 0;
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt(); //배열의 길이, 1만 이하
+        M = scan.nextInt(); //목표값, 3억 이하
+        A = new int[N]; //배열, 요소들 3만 이하
+        for (int i = 0; i < N; i++) A[i] = scan.nextInt();
+    }
+
+    static void solution() {
+        //배열에서 합이 M이 나오는 부분배열의 갯수를 구하라
+        for (int L = 0, R = 0; L < N; L++) {
+            while (R < N) {
+                if (sum + A[R] < M) {
+                    sum += A[R++];
+                } else {
+                    if (sum + A[R] == M) count++;
+                    break;
+                }
+            }
+            sum -= A[L];
+        }
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+    }
+}
