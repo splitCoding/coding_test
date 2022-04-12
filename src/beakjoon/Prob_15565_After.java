fdfbc4398138cd3f813f0a7706fe613d5d9d9a83
splitube <voicesplit@daum.net>
2022-04-12 13:16:54 +0900

    귀여운 라이언 문제 풀이 해결 (피드백 후)
    check배열을 사용하지않고 그저 라이언의 갯수만 확인하여 진행

---
+++ src/beakjoon/Prob_15565_After.java
@@ -0,0 +1,70 @@
+package beakjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Prob_15565_After {
+    static int N, K, A[], ret = 1000001;
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt(); // 1 <= K <= N <= Math.pow(10, 6);
+        K = scan.nextInt();
+        A = new int[N]; // A[i]는 1(라이언) or 2(어피치)
+        for (int i = 0; i < N; i++) A[i] = scan.nextInt();
+    }
+
+    static void solution() {
+        //라이언이 K개 이상있는 제일 작은 연속된 부분집합의 크기를 구하라
+        int lion = 0;
+        for (int L = 0, R = 0; L < N; L++) {
+            while (lion < K && R < N) {
+                if (A[R++] == 1) lion++;
+            }
+            if (lion == K) ret = Math.min(ret, R - L);
+            if (A[L] == 1) lion--;
+        }
+        System.out.println((ret == 1000001) ? -1 : ret);
+    }
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
